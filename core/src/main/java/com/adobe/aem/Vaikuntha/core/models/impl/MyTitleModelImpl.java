363d3bed375a5fb19125852345f1fcf49f6fe2fb
Yeti Venkatrao <venkatyvry1234@gmail.com>
2026-06-08 21:49:36 +0530

    loggers

--- core/src/main/java/com/adobe/aem/Vaikuntha/core/models/impl/MyTitleModelImpl.java
+++ core/src/main/java/com/adobe/aem/Vaikuntha/core/models/impl/MyTitleModelImpl.java
@@ -4,6 +4,8 @@ import com.adobe.aem.Vaikuntha.core.models.MyTitleModel;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Model(
         adaptables = Resource.class,
@@ -15,8 +17,15 @@ public class MyTitleModelImpl implements MyTitleModel {
     private String title = "Hi this value is coming from the sling model";
 
 
+    private static final Logger log = LoggerFactory.getLogger(MyTitleModelImpl.class);
+
     @Override
     public String getMyTitle() {
+        log.trace("Trace Log");
+        log.debug("Debug log");
+        log.info("Info log");
+        log.warn("Warn log");
+        log.error("Error log");
         return title;
     }
 }
